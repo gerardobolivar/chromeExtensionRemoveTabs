0214140cdecaf8331f7402c14cb2a127c195d04b
Gerardo <gerardo.fernandezmena@ucr.ac.cr>
2024-07-06 19:40:12 -0600

    Fix: Toggle state not saved; Styling

--- tabClearer.js
+++ tabClearer.js
@@ -1,29 +1,25 @@
 import WhiteListManager from "./WhiteListManager.js";
 
-const tabs = await chrome.tabs.query({
-  url: [
-    "https://*/*",
-  ]
-});
-
-let currentTabs = async () => {
-  return await chrome.tabs.query({ url: ["https://*/*"] });
-}
-
-let myActiveTab = async () => {
-  return await chrome.tabs.query(
-    { active: true, currentWindow: true }
-  )
-}
-
+let currentTabs = async () => { return await chrome.tabs.query({ url: ["https://*/*"] })};
+let myActiveTab = async () => { return await chrome.tabs.query({active: true, currentWindow: true })};
 let myStgManager = new WhiteListManager();
+let toggleState = "false";
 const domainRegex = new RegExp("https*:\/\/[a-z0-9.)]+")
-
-
 const addBtn = document.getElementById("addBtn");
 const removeBtn = document.getElementById("removeBtn");
 const printBtn = document.getElementById("printBtn");
 const clearBtn = document.getElementById("clearBtn");
+const showToggle = document.getElementById("showToggle");
+const domListElement = document.getElementById("listaTabs");
+const TOOGLE_ID = "toggle";
+
+if(localStorage.getItem(TOOGLE_ID) != null){
+  InitializeToggle()
+}else{
+  createToggleState(TOOGLE_ID)
+}
+
+showListAtPopUp();
 
 function onClearTabs() {
   console.log("Tabs cleared!");
@@ -31,6 +27,49 @@ function onClearTabs() {
 
 function onClearError(error) {
   console.error(error);
+}
+
+function populateList() {
+  let whiteListItems = myStgManager.getWhiteList();
+  domListElement.replaceChildren();
+  whiteListItems.forEach((e, index) => {
+    let node = document.createElement("li");
+    node.setAttribute("id", `tab${index}`)
+    node.append(e)
+    domListElement.appendChild(node);
+  })
+}
+
+function createToggleState(){
+  localStorage.setItem(TOOGLE_ID,"false")
+}
+
+function getToggleState(){
+  return localStorage.getItem(TOOGLE_ID) == "true";
+}
+
+function InitializeToggle(){
+  toggleState = localStorage.getItem(TOOGLE_ID);
+  console.log(`Initializing toggle value to: ${toggleState}`);
+}
+
+function updateToggleState(value){
+  localStorage.setItem(TOOGLE_ID,value)
+  toggleState = value;
+}
+
+function showListAtPopUp(){
+  populateList();
+  console.log(`Now we are at showListAtPopUp: toggleState: ${toggleState}`);
+  console.log(`toggleState == "true": ${toggleState == "true"}`);
+  if(toggleState == "true"){
+    showToggle.checked = true;
+    updateToggleState("true");
+    domListElement.style.display = "block";
+  }else{
+    domListElement.style.display = "none";
+    updateToggleState("false")
+  }
 }
 
 addBtn.addEventListener("click", () => {
@@ -55,19 +94,6 @@ removeBtn.addEventListener("click", () => {
   })
 });
 
-printBtn.addEventListener("click", () => {
-  //myStgManager.printToConsole();
-  let whiteListItems = myStgManager.getWhiteList();
-  let domListElement = document.getElementById("listaTabs");
-  domListElement.replaceChildren();
-  whiteListItems.forEach((e, index) => {
-    let node = document.createElement("li");
-    node.setAttribute("id", `tab${index}`)
-    node.append(e)
-    domListElement.appendChild(node);
-  })
-});
-
 clearBtn.addEventListener("click", () => {
   let tabsToSave = myStgManager.getWhiteList();
   let idsToRemove = []
@@ -83,3 +109,15 @@ clearBtn.addEventListener("click", () => {
   }
   )
 });
+
+showToggle.addEventListener("click", ()=>{
+  populateList();
+  if(showToggle.checked == true){
+    domListElement.style.display = "block"
+    updateToggleState("true")
+  }else{
+    domListElement.style.display = "none";
+    updateToggleState("false")
+  }
+})
+
